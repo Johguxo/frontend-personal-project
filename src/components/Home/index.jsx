d1cd5c48583b3c5f690fd59079761c48646163bd
Johguxo <jgonzalesi@uni.pe>
2022-05-17 08:16:13 -0500

    feat: Create new components

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Button } from '@rneui/base'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hola</Text>
+            <Button title='Welcome'/>
+        </View>
+    )
+}
+
+export default Home
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Button } from '@rneui/base'
+
+const LogIn = () => {
+    return (
+        <View>
+            <Text>Hola</Text>
+            <Button title='Welcome'/>
+        </View>
+    )
+}
+
+export default LogIn
